92674eaf7c7f0ba2afbd70fc83e8f0ea1835a1df
Asbjørn Enge <asbjorn@hanafjedle.net>
2013-02-18 15:28:47 +0100

    A few fixes

--- js/__init__.js
+++ js/__init__.js
@@ -1,30 +1,33 @@
 /* JavaScript code */
 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
 var cam_video_id = "camsource"
 
 window.addEventListener('DOMContentLoaded', function() {
     // Assign the <video> element to a variable
     var video = document.getElementById(cam_video_id);
-
+    var options = {
+        "audio": false,
+        "video": true
+    };
     // Replace the source of the video element with the stream from the camera
     if (navigator.getUserMedia) {
-        navigator.getUserMedia('video', successCallback, errorCallback);
+        navigator.getUserMedia(options, function(stream) {
+            video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+        }, function(error) {
+            console.log(error)
+        });
         // Below is the latest syntax. Using the old syntax for the time being for backwards compatibility.
         // navigator.getUserMedia({video: true}, successCallback, errorCallback);
-        function successCallback(stream) {
-            video.src = stream;
-        }
-        function errorCallback(error) {
-            console.error('An error occurred: [CODE ' + error.code + ']');
-            return;
-        }
     } else {
-        console.log('Native web camera streaming (getUserMedia) is not supported in this browser.');
+        $("#qr-value").text('Native web camera streaming (getUserMedia) is not supported in this browser.')
         return;
     }
 }, false);
 
 $(document).ready(function() {
+    if (!navigator.getUserMedia) return;
     cam = camera(cam_video_id);
     cam.start()
 })
